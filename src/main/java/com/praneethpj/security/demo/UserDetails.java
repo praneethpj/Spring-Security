2fa7c7f9b52e1d22aef6076b4711d28a7c6098ac
praneethpj <ifeathern@gmail.com>
2022-07-03 14:31:01 +0530

    added user details

--- src/main/java/com/praneethpj/security/demo/UserDetails.java
+++ src/main/java/com/praneethpj/security/demo/UserDetails.java
@@ -1,9 +1,16 @@
 package com.praneethpj.security.demo;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Setter
+@Getter
 public class UserDetails {
 
     @Id
@@ -12,45 +19,4 @@ public class UserDetails {
     private String password;
     private String role;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", role='" + role + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
